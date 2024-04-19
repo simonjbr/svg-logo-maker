6f8400add22a66445440c13b1d41ab26650ad4f7
Simon Brown <simonandon@live.com.au>
2024-04-19 11:48:28 +0800

    Imported inquirer and created array of questions

---
+++ index.js
@@ -0,0 +1,26 @@
+// require necessary modules
+const inquirer = require('inquirer');
+
+const questions = [
+	{
+		type: 'input',
+		message: 'Please enter the text for your logo (up to three characters)',
+		name: 'text',
+	},
+	{
+		type: 'input',
+		message: 'Please enter the text color (keyword or hexadecimal code)',
+		name: 'textColor',
+	},
+	{
+		type: 'list',
+		message: 'Please choose the shape:',
+		name: 'shape',
+		choices: ['circle', 'triangle', 'square'],
+	},
+	{
+		type: 'input',
+		message: 'Please enter the shape color (keyword or hexadecimal code)',
+		name: 'shapeColor',
+	},
+];
