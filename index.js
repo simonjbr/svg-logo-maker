fb7fcf44ccd8a954a8b3cf741503180ec65ebc7a
Simon Brown <simonandon@live.com.au>
2024-04-20 11:22:50 +0800

    Altered Shape class and all child classes to reflect example in assignment README. Altered index.js to work with updated classes

--- index.js
+++ index.js
@@ -31,7 +31,14 @@ const questions = [
 inquirer
 	.prompt(questions)
 	.then((res) => {
-		const shape = new shapes[res.shape](res.text, res.textColor, res.shape, res.shapeColor);
+		const shape = new shapes[res.shape]();
+		shape.setColor(res.shapeColor);
 
-		fs.writeFile(`./examples/${res.shape}.svg`, shape.render(), (error) => console.error(error));
+		const svg =
+`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+	${shape.render()}
+	<text x="150" y="125" font-size="60" text-anchor="middle" fill="${res.textColor}">${res.text}</text>
+</svg>`
+
+		fs.writeFile(`./examples/${res.shape}.svg`, svg, (error) => console.error(error));
 	});
